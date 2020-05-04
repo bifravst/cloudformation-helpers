8e47da8914fa0b0afbddd9876de445fb344d2e15
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-05-04 14:17:30 +0200

    feat(stackoutput): pass CloudFormation instance
    BREAKING CHANGE: this changes the stackOutput signature

--- src/stackOutput.ts
+++ src/stackOutput.ts
@@ -4,15 +4,14 @@ import { toObject } from './toObject'
 /**
  * Returns the outputs of a CloudFormation stack as an object
  */
-export const stackOutput = async <T extends { [key: string]: string }>(args: {
-	stackId: string
-	region?: string
-}): Promise<T> => {
-	const { region, stackId } = args
-	const cf = new CloudFormation({ region })
-	const { Stacks } = await cf.describeStacks({ StackName: stackId }).promise()
+export const stackOutput = (cf: CloudFormation) => async <
+	T extends { [key: string]: string }
+>(
+	StackName: string,
+): Promise<T> => {
+	const { Stacks } = await cf.describeStacks({ StackName }).promise()
 	if (!Stacks || !Stacks.length || !Stacks[0].Outputs) {
-		throw new Error(`Stack ${stackId} not found.`)
+		throw new Error(`Stack ${StackName} not found.`)
 	}
 	return toObject(Stacks[0].Outputs) as T
 }
