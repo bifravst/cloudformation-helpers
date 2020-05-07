577fbf3808d74bba23517f6f6cf07c7b16d069ba
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-05-07 16:18:01 +0200

    feat: make prefix configurable
    BREAKING CHANGE: default behaviour changes
    
    You now must pass a prefix for React style
    env definitions

--- src/objectToEnv.ts
+++ src/objectToEnv.ts
@@ -1,14 +1,15 @@
 import * as changeCase from 'change-case'
 import * as os from 'os'
 
-const toEnvKey = (key: string) => `REACT_APP_${changeCase.constantCase(key)}`
+const toEnvKey = (key: string, prefix?: string) =>
+	`${prefix ?? ''}${changeCase.constantCase(key)}`
 
 /**
  * Returns the stack outputs as create-react-app environment variables
  */
-export const objectToEnv = (obj: object) =>
+export const objectToEnv = (obj: object, prefix?: string) =>
 	Object.entries(obj).reduce(
 		(env, [OutputKey, OutputValue]) =>
-			`${env}${toEnvKey(OutputKey || '')}=${OutputValue}${os.EOL}`,
+			`${env}${toEnvKey(OutputKey || '', prefix)}=${OutputValue}${os.EOL}`,
 		'',
 	)
